571c78f3cda1c9686b6d3f943907358473edaadb
marathonbill0x <marathonbill0x@gmail.com>
2024-02-06 19:23:48 -0800

    initial commit

---
+++ app/_components/form.tsx
@@ -0,0 +1,97 @@
+import { useState } from 'react';
+
+export default function Form() {
+  const [formData, setFormData] = useState({
+    username: '',
+    packageName: '',
+    tshirtSize: '',
+    address: '',
+    email: '',
+    subscribe: false,
+  });
+
+  const [showForm, setShowForm] = useState(true)
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setFormData({
+      ...formData,
+      [name]: type === 'checkbox' ? checked : value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Send the data to our API route
+    const res = await fetch('/api/sendToGoogleSheet', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+    console.log(data);
+
+    setShowForm(false)
+
+
+    // Reset form or handle next steps here
+  };
+
+  return (
+    <>
+    { showForm ? <form onSubmit={handleSubmit} className="flex flex-col items-center w-100">
+      <input
+        type="text"
+        name="username"
+        placeholder="Username for X/Twitter"
+        value={formData.username}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="packageName"
+        placeholder="Name for Package"
+        value={formData.packageName}
+        onChange={handleChange}
+      />
+      <select name="tshirtSize" value={formData.tshirtSize} onChange={handleChange}>
+        <option value="">Select T-Shirt Size</option>
+        <option value="S">S</option>
+        <option value="M">M</option>
+        <option value="L">L</option>
+        <option value="XL">XL</option>
+        <option value="XL">XXL</option>
+        <option value="XL">XXL</option>
+
+      </select>
+      <input
+        type="text"
+        name="address"
+        placeholder="Shipping Address"
+        value={formData.address}
+        onChange={handleChange}
+      />
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={formData.email}
+        onChange={handleChange}
+      />
+    <label className="flex items-center justify-center m-5 font-black">
+        <input
+          type="checkbox"
+          name="subscribe"
+          id="checkbox"
+          checked={formData.subscribe}
+          onChange={handleChange}
+        /> 
+        Click To Subscribe!
+    </label>
+      <button id="submit" type="submit">Submit</button>
+    </form> : <h1>Thank You For Submitting!</h1>}
+    </>
+  );
+}
---
+++ app/api/sendToGoogleSheet/route.js
@@ -0,0 +1,55 @@
+// app/api/sendToGoogleSheet/route.js
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+import { JWT } from 'google-auth-library'
+
+export async function POST(req, res) {
+  try {
+    const body = await req.json()
+    const {username, packageName, tshirtSize, address, email, subscribe } = body
+
+    const SCOPES = [
+      'https://www.googleapis.com/auth/spreadsheets',
+      'https://www.googleapis.com/auth/drive.file',
+    ];
+
+    // Authenticate with the Google Sheets API
+    const jwt = new JWT({
+      email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+      key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      scopes: SCOPES,
+    });
+
+    console.log(tshirtSize)
+
+    const doc = new GoogleSpreadsheet('14zTvdquPQTNeAqqp3_8oxvpZN0Jp-vvYUJJCsULNhPc', jwt)
+
+    await doc.loadInfo(); // loads document properties and worksheets
+
+    const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
+    // Add a row with the form data
+    const result = await sheet.addRow({
+      Username: username,
+      Name: packageName,
+      ShirtSize: tshirtSize,
+      Address: address,
+      Email: email,
+      Subscribed: subscribe ? 'Yes' : 'No',
+    });
+
+    // Return a JSON response
+    return new Response(JSON.stringify({ message: 'Success' }), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    return new Response(JSON.stringify({ error: 'Failed to update Google Sheet', details: error.toString() }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+}
